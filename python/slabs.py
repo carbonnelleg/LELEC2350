d1b80b2ce0e0ced71ca0bd13cedeaa457a827572
carbonnelleg <gautier.carbonnelle@student.uclouvain.be>
2025-03-05 14:22:09 +0100

    Final version of dispersion presentation

---
+++ python/slabs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  3 18:36:18 2025
+
+@author: carbonnelleg
+"""
+
+import numpy as np
+
+z_0 = 0.
+z_max = 10.
+z_step = 1.0e-3
+z = np.arange(z_min, z_max, z_step)
