3bee2dc3c87dd426c95d87cd3c69871037ba6847
carbonnelleg <gautier.carbonnelle@student.uclouvain.be>
2025-03-10 18:02:24 +0100

    first version of fully functional code

--- python/Slabs/slabs.py
+++ python/Slabs/slabs.py
@@ -23,11 +23,11 @@ z_max = 10.
 z_step = 4.0e-3
 z = np.arange(z_min, z_max, z_step)
 
-slab1_start, slab1_end = 2.5, 4.
+slab1_start, slab1_end = 2., 4.
 slab2_start, slab2_end = 6., 8.
 
 t_min = 0.
-t_max = 1.0e-7
+t_max = 1.5e-7
 t_step = 1.0e-10
 t = np.arange(t_min, t_max, t_step)
 t_indices = np.arange(t.size)
@@ -59,7 +59,7 @@ rho_prime = -rho
 tau_prime = 1 + rho_prime
 
 
-def update_slab(slab1_start, slab1_end, slab2_start, slab2_end):
+def update_slab(d1, d2, d3, d4):
     """
     Notes
     -----
@@ -78,71 +78,60 @@ def update_slab(slab1_start, slab1_end, slab2_start, slab2_end):
     solving for the other forward and backward fields.
     """
     # Define air and slab region
-    is_slab = ((z > slab1_start) & (z < slab1_end)) | \
-              ((z > slab2_start) & (z < slab2_end))
+    is_slab = ((z > d1) & (z < d2)) | \
+              ((z > d3) & (z < d4))
 
     # Define local variables for 5 consecutive layers
-    z_1 = z[(z <= slab1_start).nonzero()]
-    z_2 = z[((z > slab1_start) & (z < slab1_end)).nonzero()] - slab1_start
-    z_3 = z[((z >= slab1_end) & (z <= slab2_start)).nonzero()] - slab1_end
-    z_4 = z[((z > slab2_start) & (z < slab2_end)).nonzero()] - slab2_start
-    z_5 = z[(z >= slab2_end).nonzero()] - slab2_end
+    z_local = [
+        z[(z <= d1).nonzero()],
+        z[((z > d1) & (z < d2)).nonzero()] - d1,
+        z[((z >= d2) & (z <= d3)).nonzero()] - d2,
+        z[((z > d3) & (z < d4)).nonzero()] - d3,
+        z[(z >= d4).nonzero()] - d4
+    ]
 
     # Define wavenumber in both media, only dependent on freq
     k_air = 2*np.pi*freqs*np.sqrt(eps_air * mu_0)
     k_slab = 2*np.pi*freqs*np.sqrt(eps_slab * mu_0)
 
-    # Perform a time shift such that wave starts at z=0, t=0
-    tot_delay = 0.
-    # Add delay due to air propagation
-    tot_delay += (slab1_start + slab2_start - slab1_end) * \
-        np.sqrt(eps_air * mu_0)
-    # Add delay due to slab propagation
-    tot_delay += (slab1_end - slab1_start + slab2_end - slab2_start) * \
-        np.sqrt(eps_slab * mu_0)
+    # Calculate Gamma at z=0
+    Gamma = np.zeros_like(freqs, dtype=np.complex128)
+    for (k, l, r) in zip(
+        [k_slab, k_air, k_slab, k_air],
+        [d4 - d3, d3 - d2, d2 - d1, d1],
+        [rho_prime, rho, rho_prime, rho]):
+
+        Gamma = np.exp(-2j*k*l) * (r + Gamma) / (1 + r*Gamma)
 
     # Calculate forward (A) and backward (B) fields, only dependent on freq
-    # A_5 is time-delayed by tot_delay
-    # A_i and B_i are shifted wrt A_{i+1} and B_{i+1}
-    A_5 = np.exp(-2j*np.pi*freqs*tot_delay)
-    A_4 = 1/tau_prime * A_5 * \
-        np.exp(1j*k_slab*(slab2_end-slab2_start))
-    B_4 = rho_prime/tau_prime * A_5 * \
-        np.exp(-1j*k_slab*(slab2_end-slab2_start))
-    A_3 = (1/tau * A_4 + rho/tau * B_4) * \
-        np.exp(1j*k_air*(slab2_start-slab1_end))
-    B_3 = (rho/tau * A_4 + 1/tau * B_4) * \
-        np.exp(-1j*k_air*(slab2_start-slab1_end))
-    A_2 = (1/tau_prime * A_3 + rho_prime/tau_prime * B_3) * \
-        np.exp(1j*k_slab*(slab1_end-slab1_start))
-    B_2 = (rho_prime/tau_prime * A_3 + 1/tau_prime * B_3) * \
-        np.exp(-1j*k_slab*(slab1_end-slab1_start))
-    A_1 = (1/tau * A_2 + rho/tau * B_2) * \
-        np.exp(1j*k_air*slab1_start)
-    B_1 = (rho/tau * A_2 + 1/tau * B_2) * \
-        np.exp(-1j*k_air*slab1_start)
+    A = np.zeros((5, freqs.size), dtype=np.complex128)
+    B = np.zeros((5, freqs.size), dtype=np.complex128)
+    A[0] += 1.
+    B[0] = A[0] * Gamma
+    for i, (k, l, r, t) in enumerate(zip(
+        [k_air, k_slab, k_air, k_slab],
+        [d1, d2 - d1, d3 - d2, d4 - d3],
+        [rho_prime, rho, rho_prime, rho],
+            [tau_prime, tau, tau_prime, tau])):
 
-    # Calculate waves to the right and to the left for all z using propagation
-    E_1r = A_1*np.exp(-1j*np.outer(k_air, z_1)).T
-    E_1l = B_1*np.exp(1j*np.outer(k_air, z_1)).T
-    E_2r = A_2*np.exp(-1j*np.outer(k_slab, z_2)).T
-    E_2l = B_2*np.exp(1j*np.outer(k_slab, z_2)).T
-    E_3r = A_3*np.exp(-1j*np.outer(k_air, z_3)).T
-    E_3l = B_3*np.exp(1j*np.outer(k_air, z_3)).T
-    E_4r = A_4*np.exp(-1j*np.outer(k_slab, z_4)).T
-    E_4l = B_4*np.exp(1j*np.outer(k_slab, z_4)).T
-    E_5r = A_5*np.exp(-1j*np.outer(k_air, z_5)).T
-    E_5l = np.zeros_like(E_5r)
-    
-    # Concatenate both fields and perform IDFT
-    E_r = np.concatenate((E_1r, E_2r, E_3r, E_4r, E_5r), axis=0) 
+        A[i+1] = 1/t * np.exp(-1j*k*l) * A[i] + r/t * np.exp(1j*k*l) * B[i]
+        B[i+1] = r/t * np.exp(-1j*k*l) * A[i] + 1/t * np.exp(1j*k*l) * B[i]
+
+    # Calculate waves to the right (E_r) and to the left (E_l) for all z using propagation
+    E_r = []
+    E_l = []
+    for i, k in enumerate([k_air, k_slab, k_air, k_slab, k_air]):
+        E_r.append(A[i] * np.exp(-1j*np.outer(k, z_local[i])).T)
+        E_l.append(B[i] * np.exp(1j*np.outer(k, z_local[i])).T)
+
+    # Concatenate both fields and perform
+    E_r = np.concatenate(E_r, axis=0)
     E_r = np.fft.ifft(np.fft.ifftshift(E_r * spect, axes=1), norm='forward')
-    E_r /= np.max(np.abs(E_r))
-    E_l = np.concatenate((E_1l, E_2l, E_3l, E_4l, E_5l), axis=0)
+    E_l = np.concatenate(E_l, axis=0)
     E_l = np.fft.ifft(np.fft.ifftshift(E_l * spect, axes=1), norm='forward')
-    E_l /= np.max(np.abs(E_r))
 
     return is_slab, E_r, E_l
+
 
 is_slab, E_r, E_l = update_slab(
     slab1_start, slab1_end, slab2_start, slab2_end)
